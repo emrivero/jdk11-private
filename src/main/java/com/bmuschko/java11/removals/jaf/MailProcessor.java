346ac0aaa894e68ec2976a254793b0994ca9aa4c
root <root@29678baeaa29>
2023-01-13 10:13:09 +0000

    Fixes from devaid. Reparation 63c12e644e9f9e79a06647b7

--- src/main/java/com/bmuschko/java11/removals/jaf/MailProcessor.java
+++ src/main/java/com/bmuschko/java11/removals/jaf/MailProcessor.java
@@ -1,14 +1,13 @@
 package com.bmuschko.java11.removals.jaf;
-
 import javax.activation.MailcapCommandMap;
-
+import java.util.logging.Logger;
 public class MailProcessor {
-    public void printMimeTypes() {
-        MailcapCommandMap mailcapCommandMap = new MailcapCommandMap();
-        String[] mimeTypes = mailcapCommandMap.getMimeTypes();
-
-        for (String mime : mimeTypes) {
-            System.out.println(mime);
-        }
+  private static final Logger logger=Logger.getLogger(MailProcessor.class.getName());
+  public void printMimeTypes(){
+    MailcapCommandMap mailcapCommandMap=new MailcapCommandMap();
+    String[] mimeTypes=mailcapCommandMap.getMimeTypes();
+    for (    String mime : mimeTypes) {
+      logger.info("" + mime);
     }
+  }
 }
