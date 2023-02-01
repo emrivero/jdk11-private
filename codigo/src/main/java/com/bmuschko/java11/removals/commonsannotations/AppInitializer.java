755cc37f051d216a184b7f1da28b0ae782e16c8c
root <root@dd1b0e674806>
2023-02-01 16:50:32 +0000

    Fixes from devaid. Reparation 63da98069c44e06c761dc655

--- codigo/src/main/java/com/bmuschko/java11/removals/commonsannotations/AppInitializer.java
+++ codigo/src/main/java/com/bmuschko/java11/removals/commonsannotations/AppInitializer.java
@@ -1,10 +1,9 @@
 package com.bmuschko.java11.removals.commonsannotations;
-
 import javax.annotation.PostConstruct;
-
+import java.util.logging.Logger;
 public class AppInitializer {
-    @PostConstruct
-    public void upAndRunning() {
-        System.out.println("The application has been started successfully!");
-    }
+  private static final Logger logger=Logger.getLogger(AppInitializer.class.getName());
+  @PostConstruct public void upAndRunning(){
+    logger.info("The application has been started successfully!");
+  }
 }
